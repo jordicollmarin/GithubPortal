34f10269464ff5091ec0ab38b6cee96277298ab9
Jordi Coll Marin <jorcollmar@gmail.com>
2020-10-31 12:57:22 +0100

    Added domain and data modules. Added splash theme and App launcher icons.

---
+++ app/src/main/java/cat/jorcollmar/githubportal/GithubPortalActivity.kt
@@ -0,0 +1,18 @@
+package cat.jorcollmar.githubportal
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cat.jorcollmar.githubportal.databinding.ActivityGithubPortalBinding
+
+class GithubPortalActivity : AppCompatActivity() {
+    lateinit var binding: ActivityGithubPortalBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Replace Splash theme with a NoActionBar theme
+        setTheme(R.style.Theme_GithubPortal_NoActionBar)
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityGithubPortalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
